5022edd5a32d5bf767e11504be4fbc9f1d0cedbe
Inioluwa Eniola <inioluwae04@gmail.com>
2026-03-12 12:06:02 -0400

    finished cart and checkout

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import ProductDetail from './pages/ProductDetail'
-import AuthContext from './context/AuthContext'
+import AuthProvider from './context/AuthContext'
+import CartProvider from './context/CartContext'
 
 function App() {
   return(
-    <AuthContext>
-      <div className='app'>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/auth" element={<Auth />}/>
-          <Route path="/checkout" element={<Checkout />}/>
-          <Route path="/products/:id" element={<ProductDetail />}/>
-        </Routes>
-      </div>
-    </AuthContext>
+    <AuthProvider>
+      <CartProvider>
+        <div className='app'>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/auth" element={<Auth />}/>
+            <Route path="/checkout" element={<Checkout />}/>
+            <Route path="/products/:id" element={<ProductDetail />}/>
+          </Routes>
+        </div>
+      </CartProvider>
+    </AuthProvider>
   )
 }
 export default App
